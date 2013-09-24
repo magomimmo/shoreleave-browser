7e8ad8b4e46598abc27efc45d3248c415a84b1be
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-09-24 23:09:54 +0200

    added group-id

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject shoreleave/shoreleave-browser "0.3.1-SNAPSHOT"
+(defproject org.clojars.magomimmo/shoreleave-browser "0.3.1-SNAPSHOT"
   :description "A smarter client-side with ClojureScript : Shoreleave's enhanced browser utilities"
   :url "http://github.com/shoreleave"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "See the notice in README.mkd or details in LICENSE_epl.html"}
-  :dependencies [[shoreleave/shoreleave-core "0.3.1-SNAPSHOT"]])
+  :dependencies [[org.clojars.magomimmo/shoreleave-core "0.3.1-SNAPSHOT"]])
 
