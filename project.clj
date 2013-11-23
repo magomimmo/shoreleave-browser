21c0a3400408b09416eaff92aa79e6d17d7304cb
magomimmo <mimmo.cosenza@gmail.com>
2013-11-23 08:54:42 +0100

    prepared for clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.magomimmo/shoreleave-browser "0.3.1-SNAPSHOT"
+(defproject org.clojars.magomimmo/shoreleave-browser "0.3.1"
   :description "A smarter client-side with ClojureScript : Shoreleave's enhanced browser utilities"
-  :url "https://github.com/magomimmo/shoreleave-browser/tree/clojars"
+  :url "https://github.com/magomimmo/shoreleave-browser/tree/0.3.1"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
