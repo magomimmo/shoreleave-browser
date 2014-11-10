9f1bf94ddae0c20ebb54d1038afb4fa4484ae763
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2014-11-10 22:03:50 +0100

    bump to 0.3.1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.clojars.magomimmo/shoreleave-browser "0.3.1-SNAPSHOT"
+(defproject org.clojars.magomimmo/shoreleave-browser "0.3.1"
   :description "A smarter client-side with ClojureScript : Shoreleave's enhanced browser utilities"
   :url "https://github.com/magomimmo/shoreleave-browser/tree/clojars"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "See the notice in README.mkd or details in LICENSE_epl.html"}
-  :dependencies [[org.clojars.magomimmo/shoreleave-core "0.3.1-SNAPSHOT"]])
+  :dependencies [[org.clojars.magomimmo/shoreleave-core "0.3.1"]])
 
